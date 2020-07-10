ee3787e6f8088d96242e0b2bb614f05b5bafc718
xdSi <3450236968@qq.com>
2020-07-11 00:52:14 +0800

    feat: 初始化工程

---
+++ js/content-script.js
@@ -0,0 +1,23 @@
+﻿
+// 接收来自后台的消息
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
+{
+	console.log('收到来自 ' + (sender.tab ? "content-script(" + sender.tab.url + ")" : "popup或者background") + ' 的消息：', request);
+	if(request.cmd == 'update_font_size') {
+		let str = document.body.innerHTML;
+		const replaceData = request.replaceData;
+		const targetData = request.targetData;
+		const isChecked = request.isChecked;
+		console.log(isChecked);
+		if(isChecked) {
+			 str = str.replace(new RegExp(replaceData, 'g'), targetData);
+		} else {
+			str = str.replace(replaceData, targetData);
+		}
+		document.body.innerHTML = str;
+	}
+	else {
+		tip(JSON.stringify(request));
+		sendResponse('我收到你的消息了：'+JSON.stringify(request));
+	}
+});
---
+++ js/popup.js
@@ -0,0 +1,47 @@
+
+
+// 获取当前选项卡ID
+function getCurrentTabId(callback)
+{
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs)
+	{
+		if(callback) callback(tabs.length ? tabs[0].id: null);
+	});
+}
+
+// 向content-script主动发送消息
+function sendMessageToContentScript(message, callback)
+{
+	getCurrentTabId((tabId) =>
+	{
+		chrome.tabs.sendMessage(tabId, message, function(response)
+		{
+			if(callback) callback(response);
+		});
+	});
+}
+
+
+// 修改内容
+$('#update_font_size').click(() => {
+	const replaceData = $('#example').val();
+	const targetData = $('#example2').val();
+	const isChecked = $('#four').prop('checked')
+	sendMessageToContentScript({cmd:'update_font_size', size: 42, replaceData, targetData, isChecked}, function(response){});
+});
+
+
+var animateButton = function(e) {
+  e.preventDefault;
+  e.target.classList.remove('animate');
+  e.target.classList.add('animate');
+  setTimeout(function(){
+    e.target.classList.remove('animate');
+  },300);
+};
+
+var classname = document.getElementsByClassName("bubbly-button");
+
+for (var i = 0; i < classname.length; i++) {
+  classname[i].addEventListener('click', animateButton, false);
+}
