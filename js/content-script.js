6ab588842d617d90d4fadd1b49ada6ba81d23474
xdSi <3450236968@qq.com>
2020-07-15 22:43:20 +0800

    perf: 优化体验

--- js/content-script.js
+++ js/content-script.js
@@ -2,23 +2,27 @@
 // 接收来自后台的消息
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
 {
-	if(request.cmd == 'replace_cmd') {
-		let str = document.body.innerHTML;
-		const replaceData = request.replaceData;  // 被替换的内容
+	const cmd  = request.cmd;
+	const replaceData = request.replaceData;  // 被替换的内容
+	const regex = RegExp(replaceData, 'gi');
+	var container = document.body
+	if(cmd == 'replace_cmd') {
 		const targetData = request.targetData; // 替换的内容
-		
-		var container = document.body
-
-		var regex = RegExp(replaceData, 'gi');
 		const instance =  findAndReplaceDOMText(container, {
 			find: regex,
-			replace: function(portion, match) {
+			replace: function(portion) {
 				called = true;
-				// var el = document.createElement('span');
-				// el.innerHTML = targetData;
+				var el = document.createElement('span');
+				el.innerHTML = targetData;
 				return targetData;
 			},
 			forceContext: findAndReplaceDOMText.NON_INLINE_PROSE
+		});
+		sendResponse({ result: true, length: instance.reverts.length })
+	} else if (cmd === 'search_cmd') {
+		// todoList
+		const instance =  findAndReplaceDOMText(container, {
+			find: regex
 		});
 		sendResponse({ result: true, length: instance.reverts.length })
 	}
--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,27 @@
 
+var searchInput = document.getElementById('example');
+
+function debounce(func,wait,immediate) {
+	let timeout;
+	return function () {
+			let context = this;
+			let args = arguments;
+
+			if (timeout) clearTimeout(timeout); 
+			if (immediate) {
+					var callNow = !timeout;
+					timeout = setTimeout(() => {
+							timeout = null; 
+					}, wait)
+					if (callNow) func.apply(context, args)
+			}
+			else {
+					timeout = setTimeout(function(){ 
+							func.apply(context, args)
+					}, wait);
+			}
+	}
+}
 
 // 获取当前选项卡ID
 function getCurrentTabId(callback)
@@ -22,19 +45,35 @@ function sendMessageToContentScript(message, callback)
 }
 
 
+function inputHandler() {
+	const replaceData = $('#example').val();
+	sendMessageToContentScript({  cmd : 'search_cmd', replaceData }, function(response) {
+		if(response && response.result) {
+			chrome.notifications.create(null, {
+				type: 'image',
+				iconUrl: 'img/find.png',
+				title: '查询',
+				message: `一共找到${response.length}处替换地方`,
+				imageUrl: 'img/find.png'
+			});
+		}
+	})
+}
+
+
 // 修改内容
 $('#replace_cmd').click(() => {
 	const replaceData = $('#example').val();
 	const targetData = $('#example2').val();
 	// const isChecked = $('#four').prop('checked');
-	sendMessageToContentScript({cmd:'replace_cmd', size: 42, replaceData, targetData }, function(response){
-		if(response.result) {
+	sendMessageToContentScript({cmd:'replace_cmd', replaceData, targetData }, function(response){
+		if( response && response.result) {
 			chrome.notifications.create(null, {
 				type: 'image',
-				iconUrl: 'img/icon.png',
+				iconUrl: 'img/summer.png',
 				title: '查询',
-				message: `一共找到${response.length}处替换地方`,
-				imageUrl: 'img/icon.png'
+				message: `一共替换了${response.length}处地方`,
+				imageUrl: 'img/summer.png'
 			});
 		}
 	});
@@ -55,3 +94,10 @@ var classname = document.getElementsByClassName("bubbly-button");
 for (var i = 0; i < classname.length; i++) {
   classname[i].addEventListener('click', animateButton, false);
 }
+
+searchInput.onchange = function() {
+	debounce(inputHandler(), 500);
+};
+
+
+
